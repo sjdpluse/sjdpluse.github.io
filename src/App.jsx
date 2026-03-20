b2fa104e9114aa871424d2acd69d3b39d8a970e1
Sajad <sajadalimohammadi394@gmail.com>
2026-03-21 00:41:05 +0430

    Update copyright year from 2025 to 2026

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
             <center>
               <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
               <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-                © 2025{" "}
+                © 2026{" "}
                 <a href="https://flowbite.com/" className="hover:underline">
                   SJDplus™
                 </a>
@@ -55,7 +55,7 @@ const ProjectPageLayout = () => (
       <center>
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-          © 2025{" "}
+          © 2026{" "}
           <a href="https://flowbite.com/" className="hover:underline">
             SJDplus™
           </a>
